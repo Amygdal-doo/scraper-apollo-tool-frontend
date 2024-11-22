704f01900ec7874070e1275ed8a6adc283dfbba0
AisaBektas <bektasaisa@gmail.com>
2024-11-22 17:38:38 +0100

    Disable scrape text option if there is no scraped link...

--- components/ScrapeOptions.tsx
+++ components/ScrapeOptions.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Accordion,
   AccordionContent,
@@ -8,8 +9,10 @@ import { ScrapeLinksForm } from "./ScrapeLinksForm";
 import ScrapeTextContent from "./ScrapeTextContent";
 import { ScrapeApolloForm } from "./ScrapeApolloForm";
 import ScrapeApolloContent from "./ScrapeApolloContent";
+import { useScrapeLinks } from "@/providers/ScrapeLinksProvider";
 
 export function ScrapeOptions() {
+  const { scrapedLinksData } = useScrapeLinks();
   return (
     <Accordion type="multiple" className="w-full">
       <AccordionItem value="item-1">
@@ -20,14 +23,16 @@ export function ScrapeOptions() {
           <ScrapeLinksForm />
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger className="font-bold text-lg">
-          Scrape Text
-        </AccordionTrigger>
-        <AccordionContent>
-          <ScrapeTextContent />
-        </AccordionContent>
-      </AccordionItem>
+      {scrapedLinksData.length > 0 && (
+        <AccordionItem value="item-2">
+          <AccordionTrigger className="font-bold text-lg">
+            Scrape Text
+          </AccordionTrigger>
+          <AccordionContent>
+            <ScrapeTextContent />
+          </AccordionContent>
+        </AccordionItem>
+      )}
       <AccordionItem value="item-3">
         <AccordionTrigger className="font-bold text-lg">
           Scrape Apollo
